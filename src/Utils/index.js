d2b28948f15817d37c26c1a9620aa87c1a31d9e1
Ubuntu <ubuntu@ip-172-31-11-31.ap-south-1.compute.internal>
2022-08-22 08:00:06 +0000

    Removed heroku api

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,3 +1,2 @@
 // #production
-// export const BASE_URL = 'http://43.205.174.245/api'
-export const BASE_URL = 'https://ollbackend.herokuapp.com/api'
+export const BASE_URL = 'http://43.205.174.245/api'
