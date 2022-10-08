84c885c3af59864108219ee0b21ce84f3cc61673
Anekant <Anekantjainsagar@gmail.com>
2022-10-08 16:29:32 +0530

    Changed backend url

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,4 +1,4 @@
 // #production
-export const BASE_URL = "http://43.204.163.216/api";
+export const BASE_URL = "https://crm.oll.co/api/";
 // export const BASE_URL = "https://ollbackend.herokuapp.com/api";
 // export const SOCKET_URL = "http://localhost:8000";
