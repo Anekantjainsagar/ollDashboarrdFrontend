5534e96405329bd2b878d4885beea08a92e607d7
Anekant <Anekantjainsagar@gmail.com>
2022-12-09 00:38:49 +0530

    aksdjf

--- src/Monitor/Utils/index.js
+++ src/Monitor/Utils/index.js
@@ -5,5 +5,5 @@ export const BASE_URL = "https://crm.oll.co/api/";
 
 // const MONITOR_BACKEND = "https://b2boll.onrender.com/api/monitor";
 // const MONITOR_BACKEND = "http://localhost:8000/api/monitor";
-const MONITOR_BACKEND = "https://crm.oll.co/api/monitor";
+const MONITOR_BACKEND = "https://crm.oll.co/api/b2b";
 export default MONITOR_BACKEND;
