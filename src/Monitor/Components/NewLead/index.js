e80990042cab890aa82ae0bbf183874078d6cf87
Anekant <Anekantjainsagar@gmail.com>
2022-12-10 19:30:50 +0530

    EMail template changes

--- src/Monitor/Components/NewLead/index.js
+++ src/Monitor/Components/NewLead/index.js
@@ -67,47 +67,47 @@ const NewLead = ({ schools, getSchools, getUsers, agents, offers }) => {
           setSchoolSaved(false);
           setofferSaved(false);
           console.log(res);
-          const options = {
-            method: "POST",
-            headers: {
-              "content-type": "text/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiMzIyYzViYi1kYzQwLTRmODctYjZiMi1iMjMyOTQyMjBiOGUiLCJ1bmlxdWVfbmFtZSI6ImluZm9Ab2xsLmNvIiwibmFtZWlkIjoiaW5mb0BvbGwuY28iLCJlbWFpbCI6ImluZm9Ab2xsLmNvIiwiYXV0aF90aW1lIjoiMDgvMDEvMjAyMiAwNDowMDo1NiIsImRiX25hbWUiOiIxMTUwNyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFETUlOSVNUUkFUT1IiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.k89dQ0gkjcZ3T8VYDz6FIbr4sisaSiSTvjLZ7FhLEAc",
-            },
-            body: JSON.stringify({
-              receivers: [
-                {
-                  customParams: [
-                    { name: "client_name", value: "name" },
-                    { name: "id", value: "id" },
-                    { name: "query_date", value: "d.slice(4, 16)" },
-                    { name: "query_time", value: "d.slice(16, 21)" },
-                    { name: "query_status", value: "e.target.value" },
-                  ],
-                  whatsappNumber: "917692045606",
-                },
-                {
-                  customParams: [
-                    { name: "client_name", value: "name" },
-                    { name: "id", value: "id" },
-                    { name: "query_date", value: "d.slice(4, 16)" },
-                    { name: "query_time", value: "d.slice(16, 21)" },
-                    { name: "query_status", value: "e.target.value" },
-                  ],
-                  whatsappNumber: "919699188188",
-                },
-              ],
-              template_name: "query_add_to_ops",
-              broadcast_name: "alert",
-            }),
-          };
-          fetch(
-            "https://live-server-11507.wati.io/api/v1/sendTemplateMessages",
-            options
-          )
-            .then((response) => response.json())
-            .then((response) => console.log(response))
-            .catch((err) => console.error(err));
+          // const options = {
+          //   method: "POST",
+          //   headers: {
+          //     "content-type": "text/json",
+          //     Authorization:
+          //       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiMzIyYzViYi1kYzQwLTRmODctYjZiMi1iMjMyOTQyMjBiOGUiLCJ1bmlxdWVfbmFtZSI6ImluZm9Ab2xsLmNvIiwibmFtZWlkIjoiaW5mb0BvbGwuY28iLCJlbWFpbCI6ImluZm9Ab2xsLmNvIiwiYXV0aF90aW1lIjoiMDgvMDEvMjAyMiAwNDowMDo1NiIsImRiX25hbWUiOiIxMTUwNyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFETUlOSVNUUkFUT1IiLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.k89dQ0gkjcZ3T8VYDz6FIbr4sisaSiSTvjLZ7FhLEAc",
+          //   },
+          //   body: JSON.stringify({
+          //     receivers: [
+          //       {
+          //         customParams: [
+          //           { name: "client_name", value: "name" },
+          //           { name: "id", value: "id" },
+          //           { name: "query_date", value: "d.slice(4, 16)" },
+          //           { name: "query_time", value: "d.slice(16, 21)" },
+          //           { name: "query_status", value: "e.target.value" },
+          //         ],
+          //         whatsappNumber: "917692045606",
+          //       },
+          //       {
+          //         customParams: [
+          //           { name: "client_name", value: "name" },
+          //           { name: "id", value: "id" },
+          //           { name: "query_date", value: "d.slice(4, 16)" },
+          //           { name: "query_time", value: "d.slice(16, 21)" },
+          //           { name: "query_status", value: "e.target.value" },
+          //         ],
+          //         whatsappNumber: "919699188188",
+          //       },
+          //     ],
+          //     template_name: "query_add_to_ops",
+          //     broadcast_name: "alert",
+          //   }),
+          // };
+          // fetch(
+          //   "https://live-server-11507.wati.io/api/v1/sendTemplateMessages",
+          //   options
+          // )
+          //   .then((response) => response.json())
+          //   .then((response) => console.log(response))
+          //   .catch((err) => console.error(err));
 
           //Sending mail
           emailjs
@@ -116,8 +116,7 @@ const NewLead = ({ schools, getSchools, getUsers, agents, offers }) => {
               "template_p4hiil9",
               {
                 name: user?.schoolName,
-                fromName: "OLL",
-                message: "I am a messgage",
+                from_name: "OLL",
                 email: email,
               },
               "SkxN6CQDdddlQ84Qj"
