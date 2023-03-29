0b9746dd7464055a8e8c39f17fe8bf2afdee0ba5
Anekant <Anekantjainsagar@gmail.com>
2023-03-29 14:35:11 +0530

    IMport buttton

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,19 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 import React, { useState, useEffect, useContext } from "react";
 import { CSVLink } from "react-csv";
 import { MdOutlineAccessTimeFilled } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import B2BContext from "../Monitor/Context/B2BContext";
 import { BASE_URL } from "../Utils/index";
+import fileDownload from "js-file-download";
+import MONITOR_BACKEND from "../Monitor/Utils";
 
 const Nav = ({ sales }) => {
   const [time, settime] = useState(new Date().toLocaleTimeString());
   const [logoutBtn, setlogoutBtn] = useState(false);
   const history = useNavigate();
   const b2b = useContext(B2BContext);
-
+  const [file, setFile] = useState();
   const [exportData, setExportData] = useState([]);
 
   useEffect(() => {
@@ -34,6 +36,60 @@ const Nav = ({ sales }) => {
         <h1>Dashboard</h1>
         <div className="time">
           <MdOutlineAccessTimeFilled size={25} color={"grey"} /> {time}
+        </div>
+        <div style={{ width: "45%", display: "flex", alignItems: "center" }}>
+          <form
+            encType="multipart/form-data"
+            method="post"
+            style={{ width: "45%" }}
+          >
+            <input
+              type="file"
+              style={{ width: "100%" }}
+              name="uploadfile"
+              onChange={(e) => {
+                setFile(e.target.files[0]);
+              }}
+            />
+            {file ? (
+              <button
+                style={{ width: "40%" }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  const formData = new FormData();
+                  formData.append("uploadfile", file);
+                  axios
+                    .post(`${BASE_URL}addExcelUsers`, formData)
+                    .then((res) => {
+                      if (res?.data[0]?._id) {
+                        setFile("");
+                      }
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }}
+              >
+                Import
+              </button>
+            ) : null}
+          </form>
+          <button
+            style={{ width: "23%" }}
+            onClick={(e) => {
+              e.preventDefault();
+              Axios({
+                url: `${MONITOR_BACKEND}/download/b2cFormat`,
+                method: "GET",
+                responseType: "blob",
+              }).then((res) => {
+                console.log(res);
+                fileDownload(res.data, "format.csv");
+              });
+            }}
+          >
+            Predefined Fomat
+          </button>
         </div>
         <div className="profileSection">
           {exportData?.length > 0 ? (
