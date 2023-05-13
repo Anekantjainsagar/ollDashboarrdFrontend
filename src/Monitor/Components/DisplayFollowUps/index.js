df66f368529e58eae6253de885eacde783051657
Anekant <Anekantjainsagar@gmail.com>
2023-05-13 18:06:11 +0530

    13 work done

--- src/Monitor/Components/DisplayFollowUps/index.js
+++ src/Monitor/Components/DisplayFollowUps/index.js
@@ -12,11 +12,42 @@ const DisplayFollowUps = ({ followUp, user, schools }) => {
   }, [user, schools]);
 
   return (
-    <div>
-      {userFollowUps.map((follow, i) => {
-        return <Bar follow={follow} schools={schools} key={i} />;
-      })}
-    </div>
+    <>
+      {userFollowUps.filter((e) => {
+        return new Date(e.startDate) > new Date(Date.now());
+      }).length > 0 ? (
+        <div
+          style={{
+            margin: "1rem 0",
+          }}
+        >
+          <h1 style={{ fontSize: "1.75rem", textAlign: "center" }}>
+            Upcoming FollowUps
+          </h1>
+          {userFollowUps
+            .filter((e) => {
+              return new Date(e.startDate) > new Date(Date.now());
+            })
+            .map((follow, i) => {
+              return <Bar follow={follow} schools={schools} key={i} />;
+            })}
+        </div>
+      ) : null}
+      <div
+        style={{
+          margin: "1rem 0",
+        }}
+      >
+        <h1 style={{ fontSize: "1.75rem", textAlign: "center" }}>FollowUps</h1>
+        {userFollowUps
+          .filter((e) => {
+            return new Date(e.startDate) <= new Date(Date.now());
+          })
+          .map((follow, i) => {
+            return <Bar follow={follow} schools={schools} key={i} />;
+          })}
+      </div>
+    </>
   );
 };
 
